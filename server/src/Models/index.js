9925d3bceda74c0402c3e10d6d2f343a18917edc
Sania Singla <singlasania7717@gmail.com>
2025-03-11 17:22:48 +0530

    final

--- server/src/Models/index.js
+++ server/src/Models/index.js
@@ -2,7 +2,19 @@ import { Snack } from './snack.Model.js';
 import { Contractor } from './contractor.Model.js';
 import { Hostel } from './hostel.Model.js';
 import { Canteen } from './canteen.Model.js';
-import { Order } from './order.Model.js';
-import { Student, OrderHistory } from './student.Model.js';
+import { Order, OrderHistory } from './order.Model.js';
+import { Student } from './student.Model.js';
+import { Admin } from './admin.Model.js';
+import { PackagedFood } from './packagedFood.Model.js';
 
-export { Student, Contractor, Canteen, Hostel, Order, OrderHistory, Snack };
+export {
+    Student,
+    Contractor,
+    Canteen,
+    Hostel,
+    Order,
+    OrderHistory,
+    Snack,
+    Admin,
+    PackagedFood,
+};

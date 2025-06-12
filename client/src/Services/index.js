d95907177130194a046aba42cd30f84c478ebfdd
kartik <kartikgoutam7@gmail.com>
2025-06-12 14:53:37 +0530

    1

--- client/src/Services/index.js
+++ client/src/Services/index.js
@@ -1,6 +1,6 @@
 import { snackService } from './snack.Service';
 import { orderService } from './order.Service';
 import { contractorService } from './contractor.Service';
-import { userService } from './user.Service';
+import { userService } from './user.service';
 
 export { snackService, orderService, contractorService, userService };
